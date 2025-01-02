c41d2538c6c08918a0cad9c6fd01b9b75c317114
SnigdhaM <smileyramya04@gmail.com>
2025-01-02 13:54:55 -0800

    with this fix, linting cl/ci should work?

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -5,10 +5,9 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.ts"],
-    languageOptions: { globals: globals.browser },
-    ...pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
