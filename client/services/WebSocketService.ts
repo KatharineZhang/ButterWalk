207d28de936f7ffc2538ba51e282b79c39698cb1
SnigdhaM <smileyramya04@gmail.com>
2025-01-28 12:09:22 -0800

    listening functionality

--- client/services/WebSocketService.ts
+++ client/services/WebSocketService.ts
@@ -1,10 +1,24 @@
-import { WebSocketMessage } from "../../server/src/api";
+import {
+  Command,
+  WebSocketMessage,
+  WebSocketResponse,
+} from "../../server/src/api";
+
+// the type of function (event handler) that will be called when a message of a certain type is received
+type WebSocketResponseHandler = (message: WebSocketResponse) => void;
 
 // Abstracts the websocket details from the react native app
 class WebSocketService {
   private websocket: WebSocket | null = null;
+  private messageHandlers: Map<Command, WebSocketResponseHandler[]> = new Map();
 
-  // Must pass in a netid to connect the generated websocket to the user
+  /**
+   * Create a new websocket connection to the server,
+   * linking a specific netid to the websocket instance created
+   *
+   * @param netid
+   * @returns
+   */
   connect(netid: string) {
     if (
       this.websocket != null &&
@@ -32,19 +46,38 @@ class WebSocketService {
       console.log("WEBSOCKET: Connected to Websocket");
       this.send({ directive: "CONNECT", netid: netid });
     };
+
     this.websocket.onmessage = (event) => {
       console.log(`WEBSOCKET: Received message => ${event.data}`);
-      // TODO: send message to component
+      const message = JSON.parse(event.data) as WebSocketResponse;
+      // send message to any component interested in this message type
+      const key =
+        message.response == "ERROR" ? message.category : message.response;
+
+      const handlersToCall: WebSocketResponseHandler[] | undefined =
+        this.messageHandlers.get(key);
+      // if there are handlers for this message type, i.e. handlersToCall is no undefined, call them
+      if (handlersToCall) {
+        handlersToCall.forEach((handler) => {
+          handler(message);
+        });
+      }
     };
+
     this.websocket.onclose = () => {
       console.log("WEBSOCKET: Disconnected from Websocket");
     };
+
     this.websocket.onerror = (error: Event) => {
       console.error(`WEBSOCKET: Error: ${(error as ErrorEvent).message}`);
     };
   }
 
-  // Send a message to the websocket
+  /**
+   * Send a message to the websocket server
+   *
+   * @param message
+   */
   send(message: WebSocketMessage) {
     if (
       this.websocket != null &&
@@ -54,6 +87,32 @@ class WebSocketService {
       return;
     }
     console.error("No websocket connection");
+  }
+
+  /**
+   * Adds a event handler to our collection. Used to update state in components
+   * when a message of certain directive type is received
+   *
+   * @param handler the function this service will call when it receives a message of response type 'directive'
+   * @param directive the response type the component is interested in
+   */
+  addListener(handler: WebSocketResponseHandler, directive: Command) {
+    // add directive to map if not already there
+    if (!this.messageHandlers.has(directive)) {
+      this.messageHandlers.set(directive, []);
+    }
+    // add to the directive's array of handlers
+    this.messageHandlers.get(directive)?.push(handler);
+  }
+
+  /**
+   * The converse of addListener. Removes a handler from the collection
+   *
+   * @param handler the handler to remove
+   * @param directive the dirctive passed in to addListener when this handler was added
+   */
+  removeListener(handler: WebSocketResponseHandler, directive: Command) {
+    this.messageHandlers.get(directive)?.filter((h) => h !== handler);
   }
 
   // Disconnect from the websocket
