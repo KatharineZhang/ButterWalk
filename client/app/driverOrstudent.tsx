2ef5036a82bb5d648817f7d7c79788074173dba4
HiyaMehta2 <hiyamehta@computingforall.org>
2025-07-18 12:07:50 -0700

    sentry connection testing button

--- client/app/driverOrstudent.tsx
+++ client/app/driverOrstudent.tsx
@@ -2,6 +2,16 @@ import { styles } from "@/assets/styles";
 import { Link } from "expo-router";
 import { View, Text, Pressable } from "react-native";
 
+import { Button } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://16af6dee84f28f5b28487764feb725a6@o4509688035016704.ingest.us.sentry.io/4509688215896064', // replace with your actual DSN
+  // enableInExpoDevelopment: true,
+  debug: true, // logs Sentry events to your console
+});
+
+
 export default function driverOrstudent() {
   return (
     <View
@@ -22,6 +32,13 @@ export default function driverOrstudent() {
       >
         To start, are you a UW Student or a UWPD Driver?
       </Text>
+      <Button
+        title="Test Sentry"
+        onPress={() => {
+          Sentry.captureException(new Error('Test error for Sentry'));
+        }}
+      />
+
       <View style={{ height: 20 }} />
       <View style={styles.buttonContainer}>
         <Link href={"/(driver)/signin"} asChild>
