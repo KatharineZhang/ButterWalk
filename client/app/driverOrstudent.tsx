62cfb2f4ef8f504272927257cc73bcf5b1928774
HiyaMehta2 <hiyamehta@computingforall.org>
2025-07-28 00:36:54 -0700

    sentry deleted

--- client/app/driverOrstudent.tsx
+++ client/app/driverOrstudent.tsx
@@ -3,13 +3,6 @@ import { Link } from "expo-router";
 import { View, Text, Pressable } from "react-native";
 
 import { Button } from 'react-native';
-import * as Sentry from '@sentry/react-native';
-
-Sentry.init({
-  dsn: 'https://16af6dee84f28f5b28487764feb725a6@o4509688035016704.ingest.us.sentry.io/4509688215896064', // replace with your actual DSN
-  // enableInExpoDevelopment: true,
-  debug: true, // logs Sentry events to your console
-});
 
 
 export default function driverOrstudent() {
