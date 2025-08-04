45fcbe40b5b8daf73629931aeef595a3c882e163
HiyaMehta2 <hiyamehta@computingforall.org>
2025-08-03 21:55:42 -0700

    crashing fixed

--- client/app.config.ts
+++ client/app.config.ts
@@ -27,7 +27,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "com.butterwalkios.butterwalk",
+    bundleIdentifier: "com.butterwalk.butterwalk",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       NSLocationWhenInUseUsageDescription:
