c61aa2a80ad9740b8a19d8f9a7d84d3ec82d8b7e
Adwita Garg <90994199+agarg09@users.noreply.github.com>
2025-09-06 20:22:32 -0700

    Clean up/commenting oauthredirect.tsx

--- client/app/oauthredirect.tsx
+++ client/app/oauthredirect.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 const Loading = () => {
   // Simple oauthredirect, no UI, but it redirects back to signin to catch
-  // the Google response from the signin (something we were missing previously)
+  // the Google response object from the signin (something we were missing previously)
   return <Redirect href={{ pathname: "/(student)/signin" }}></Redirect>;
 }
 
