6c261ead54c764deb6499a9bccef892708dc4dd9
HiyaMehta2 <hiyamehta@computingforall.org>
2025-09-12 16:51:53 -0700

    reanimated version fixed

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View } from 'react-native';
 import { styles } from "@/assets/styles";
-import { View } from "react-native";
 import Onboarding from "./onboarding";
 import { useKeepAwake } from "expo-keep-awake";
 
 export default function HomeScreen() {
   useKeepAwake();
   return (
-    <View style={styles.container}>
-      <Onboarding></Onboarding>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Onboarding />
+      </View>
+    </GestureHandlerRootView>
   );
 }
