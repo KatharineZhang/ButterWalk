a30f0490d02a63360b661e2b832b87651d27e4b9
SnigdhaM <smileyramya04@gmail.com>
2025-10-11 14:56:50 -0700

    change whichcomponent and request info to ref

--- client/app/(driver)/home.tsx
+++ client/app/(driver)/home.tsx
@@ -44,7 +44,10 @@ export type HandleRidePhase =
 
 export default function HomePage() {
   /* HOME PAGE STATE */
-  const [whichComponent, setWhichComponent] = useState<
+  const [, setWhichComponent] = useState<
+    "noRequests" | "requestsAreAvailable" | "handleRide" | "endShift"
+  >(TimeService.inServicableTime() ? "noRequests" : "noRequests");
+  const whichComponent = useRef<
     "noRequests" | "requestsAreAvailable" | "handleRide" | "endShift"
   >("noRequests");
 
@@ -104,6 +107,7 @@ export default function HomePage() {
     if (TimeService.inServicableTime()) {
       // in shift
       setWhichComponent("noRequests");
+      whichComponent.current = "noRequests";
       seeIfRidesExist();
       // see if there is an active ride request
       sendLoadRide();
@@ -114,6 +118,7 @@ export default function HomePage() {
         "Service is only available between 6:30 PM and 2:00 AM (but i'll allow it this time...)"
       );
       setWhichComponent("noRequests");
+      whichComponent.current = "noRequests";
       seeIfRidesExist();
       // see if there is an active ride request
       sendLoadRide();
@@ -137,10 +142,10 @@ export default function HomePage() {
 
   // Set start location when ride is accepted
   useEffect(() => {
-    if (whichComponent == "handleRide") {
+    if (whichComponent.current == "handleRide") {
       setStartLocation(driverLocationRef.current);
     }
-  }, [whichComponent]);
+  }, [whichComponent.current]);
 
   /* MAP STATE */
   const [, setDriverLocation] = useState<{
@@ -187,7 +192,7 @@ export default function HomePage() {
     setDriverLocation(location);
     driverLocationRef.current = location;
     // send the location to the student once the ride is accepted
-    if (whichComponent === "handleRide" && requestInfo.netid) {
+    if (whichComponent.current === "handleRide" && requestInfo.current.netid) {
       WebSocketService.send({
         directive: "LOCATION",
         id: netid,
@@ -249,9 +254,9 @@ export default function HomePage() {
     useState<number>(0);
   const [pickupToDropoffDuration, setPickupToDropoffDuration] =
     useState<number>(0);
-  const [requestInfo, setRequestInfo] = useState<RideRequest>(
-    {} as RideRequest
-  );
+  const [, setRequestInfo] = useState<RideRequest>({} as RideRequest);
+  const requestInfo = useRef<RideRequest>({} as RideRequest);
+
   const [showAcceptScreen, setShowAcceptScreen] = useState(true);
 
   const onAccept = () => {
@@ -272,7 +277,7 @@ export default function HomePage() {
     WebSocketService.send({
       directive: "VIEW_DECISION",
       driverid: netid,
-      netid: requestInfo.netid,
+      netid: requestInfo.current.netid,
       decision: "ACCEPT",
     });
   };
@@ -310,14 +315,14 @@ export default function HomePage() {
   const [isNearDropoff, setIsNearDropoff] = useState(false);
 
   const flagStudent = (reason: string) => {
-    if (!requestInfo.requestId) {
+    if (!requestInfo.current.requestId) {
       return; // TODO: handle error case where requestId is not set
     }
     // call the REPORT route
     WebSocketService.send({
       directive: "REPORT",
-      netid: requestInfo.netid, // the student netid
-      requestid: requestInfo.requestId, // the ride request id
+      netid: requestInfo.current.netid, // the student netid
+      requestid: requestInfo.current.requestId, // the ride request id
       reason,
     });
   };
@@ -334,7 +339,7 @@ export default function HomePage() {
   const completeRide = () => {
     WebSocketService.send({
       directive: "COMPLETE",
-      requestid: requestInfo.requestId as string,
+      requestid: requestInfo.current.requestId as string,
     });
   };
 
@@ -342,7 +347,7 @@ export default function HomePage() {
     WebSocketService.send({
       directive: "DRIVER_ARRIVED_AT_PICKUP",
       driverid: netid,
-      studentNetid: requestInfo.netid,
+      studentNetid: requestInfo.current.netid,
     });
   };
 
@@ -350,7 +355,7 @@ export default function HomePage() {
     WebSocketService.send({
       directive: "DRIVER_DRIVING_TO_DROPOFF",
       driverid: netid,
-      studentNetid: requestInfo.netid,
+      studentNetid: requestInfo.current.netid,
     });
   };
 
@@ -360,6 +365,7 @@ export default function HomePage() {
     setDropOffLocation({ latitude: 0, longitude: 0 });
     setDriverToPickupDuration(0);
     setPickupToDropoffDuration(0);
+    requestInfo.current = {} as RideRequest;
     setRequestInfo({} as RideRequest);
     setFlaggingAllowed(false);
     setFlagPopupVisible(false);
@@ -369,7 +375,7 @@ export default function HomePage() {
       boldText: "",
       trigger: 0,
     });
-    setWhichComponent("noRequests");
+    whichComponent.current = "noRequests";
     // Reset progress tracking states
     setPickupProgress(0);
     setDropoffProgress(0);
@@ -390,6 +396,7 @@ export default function HomePage() {
       // if successful, set the current component to "noRequests"
       resetAllFields();
       setWhichComponent("noRequests");
+      whichComponent.current = "noRequests";
       setNotifState({
         text: "Your ride was canceled",
         color: "#FFCBCB",
@@ -415,6 +422,7 @@ export default function HomePage() {
       // reset all fields
       resetAllFields();
       setWhichComponent("noRequests");
+      whichComponent.current = "noRequests";
     } else {
       // if not successful, log the error
       const errMessage = message as ErrorResponse;
@@ -432,13 +440,15 @@ export default function HomePage() {
     if ("response" in message && message.response === "RIDES_EXIST") {
       const ridesExistMessage = message as RidesExistResponse;
       if (
-        whichComponent === "noRequests" ||
-        whichComponent === "requestsAreAvailable"
+        whichComponent.current === "noRequests" ||
+        whichComponent.current === "requestsAreAvailable"
       ) {
         // if the driver is waiting for a request
         if (ridesExistMessage.ridesExist) {
           // and rides exist, set the component to "requestsAreAvailable"
           setWhichComponent("requestsAreAvailable");
+          whichComponent.current = "requestsAreAvailable";
+
           setNotifState({
             text: "New ride request available",
             color: "#C9FED0",
@@ -448,6 +458,7 @@ export default function HomePage() {
         } else {
           // if false, set the component to "noRequests"
           setWhichComponent("noRequests");
+          whichComponent.current = "noRequests";
         }
       } else {
         // if the driver is not waiting for a request, do nothing
@@ -481,6 +492,7 @@ export default function HomePage() {
       if (viewReqResponse.rideInfo) {
         // if the ride request info exists, then the view was successful
         // set the requestInfo state to the ride request info
+        requestInfo.current = viewReqResponse.rideInfo.rideRequest;
         setRequestInfo(viewReqResponse.rideInfo.rideRequest);
 
         // set the pick up and drop off locations coordinates
@@ -511,6 +523,7 @@ export default function HomePage() {
         });
         resetAllFields(); // reset all fields
         setWhichComponent("noRequests"); // go to no requests page
+        whichComponent.current = "noRequests";
       }
     } else {
       const errMessage = message as ErrorResponse;
@@ -520,6 +533,7 @@ export default function HomePage() {
         trigger: Date.now(),
       });
       setWhichComponent("noRequests"); // go to no requests page
+      whichComponent.current = "noRequests";
     }
   };
 
@@ -536,6 +550,7 @@ export default function HomePage() {
       });
       setStartLocation(driverLocationRef.current);
       setWhichComponent("handleRide");
+      whichComponent.current = "handleRide";
       WebSocketService.send({
         directive: "LOCATION",
         id: netid,
@@ -556,6 +571,7 @@ export default function HomePage() {
       });
       resetAllFields(); // reset all fields
       setWhichComponent("noRequests"); // go to no requests page
+      whichComponent.current = "noRequests";
     }
   };
 
@@ -599,6 +615,8 @@ export default function HomePage() {
         const ride = loadRideMessage.rideRequest;
         setPickUpLocation(ride.locationFrom.coordinates);
         setDropOffLocation(ride.locationTo.coordinates);
+        setStudentLocation(ride.studentLocation.coords);
+        requestInfo.current = ride;
         setRequestInfo(ride);
 
         // decide which phase to set based on the ride status
@@ -606,11 +624,15 @@ export default function HomePage() {
           case "VIEWING":
             // go to requestsAreAvailable page
             setWhichComponent("requestsAreAvailable");
+            whichComponent.current = "requestsAreAvailable";
+
             // Switch to the Let's Go page here
             setShowAcceptScreen(false);
             break;
           case "DRIVING TO PICK UP":
             setWhichComponent("handleRide");
+            whichComponent.current = "handleRide";
+
             setPhase("headingToPickup");
             break;
           case "DRIVER AT PICK UP":
@@ -624,6 +646,7 @@ export default function HomePage() {
           default:
             // if the ride is in any other status (completed), go to noRequests page
             setWhichComponent("noRequests");
+            whichComponent.current = "noRequests";
             break;
         }
         // get any wait time info
@@ -736,7 +759,7 @@ export default function HomePage() {
   /* PROGRESS TRACKING EFFECTS */
   // Track progress when driver location changes and is handling a ride
   useEffect(() => {
-    if (whichComponent === "handleRide") {
+    if (whichComponent.current === "handleRide") {
       let pickupProgress = 0;
       let dropoffProgress = 0;
 
@@ -783,7 +806,12 @@ export default function HomePage() {
         setDropoffProgress(dropoffProgress);
       }
     }
-  }, [driverLocationRef.current, phase, whichComponent, requestInfo.requestId]);
+  }, [
+    driverLocationRef.current,
+    phase,
+    whichComponent.current,
+    requestInfo.current.requestId,
+  ]);
 
   // Calculate progress based on total distance and remaining distance for non-linear tracking
   const calculateProgress = (): number => {
@@ -1007,17 +1035,17 @@ export default function HomePage() {
       </View>
 
       {/* Decide which component to render */}
-      {whichComponent === "noRequests" ? (
+      {whichComponent.current === "noRequests" ? (
         <View style={styles.homePageComponentContainer}>
           <NoRequests
             updateSideBarHeight={setCurrentComponentHeight}
             seeIfRidesExist={seeIfRidesExist}
           />
         </View>
-      ) : whichComponent === "requestsAreAvailable" ? (
+      ) : whichComponent.current === "requestsAreAvailable" ? (
         <View style={styles.homePageComponentContainer}>
           <RequestAvailable
-            requestInfo={requestInfo}
+            requestInfo={requestInfo.current}
             showAcceptScreen={showAcceptScreen}
             updateSideBarHeight={setCurrentComponentHeight}
             driverToPickupDuration={driverToPickupDuration}
@@ -1026,12 +1054,12 @@ export default function HomePage() {
             onLetsGo={onLetsGo}
           />
         </View>
-      ) : whichComponent === "handleRide" ? (
+      ) : whichComponent.current === "handleRide" ? (
         <View style={styles.homePageComponentContainer}>
           <HandleRide
             phase={phase}
             setPhase={setPhase}
-            requestInfo={requestInfo}
+            requestInfo={requestInfo.current}
             driverToPickupDuration={driverToPickupDuration}
             pickupToDropoffDuration={pickupToDropoffDuration}
             changeFlaggingAllowed={setFlaggingAllowed}
@@ -1048,7 +1076,7 @@ export default function HomePage() {
             updateSideBarHeight={setCurrentComponentHeight}
           />
         </View>
-      ) : whichComponent === "endShift" ? (
+      ) : whichComponent.current === "endShift" ? (
         <View style={styles.homePageComponentContainer}>
           <ShiftIsOver updateSideBarHeight={setCurrentComponentHeight} />
         </View>
