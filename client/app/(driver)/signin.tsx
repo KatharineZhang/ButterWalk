1547b4ba3f645e1d1984a8f7ae9a8395f967d5d0
SnigdhaM <smileyramya04@gmail.com>
2025-11-29 11:16:44 -0800

    fix driver sign in page

--- client/app/(driver)/signin.tsx
+++ client/app/(driver)/signin.tsx
@@ -1,114 +1,167 @@
-import { View, Text, TouchableOpacity, Image, Pressable } from "react-native";
-import { styles } from "../../assets/styles";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Pressable,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { Redirect } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
-// need to 'npx expo install expo-web-browser expo-auth-session expo-crypto' ON MAC
-// or 'npm i expo-auth-session@~6.0.3' on windows
-import * as Google from "expo-auth-session/providers/google";
-import { useEffect } from "react";
-import { router, useLocalSearchParams, Link } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-// Images
-// @ts-expect-error the image does exists so get rid of the error
-import logo from "@/assets/images/GoogleG.png";
-// @ts-expect-error the image does exists so get rid of the error
-// import butterWalkLogo from "@/assets/images/butterWalkLogo.png";
-import huskyCarImage from "@/assets/images/husky-car.png";
-import { SafeAreaView } from "react-native-safe-area-context";
-
-const webClientId = process.env.EXPO_PUBLIC_WEB_CLIENT_ID;
-const iosClientId = process.env.EXPO_PUBLIC_IOS_CLIENT_ID;
-const androidClientId = process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID;
+import WebSocketService, {
+  WebsocketConnectMessage,
+} from "@/services/WebSocketService";
+import { WebSocketResponse, ErrorResponse } from "../../../server/src/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
-  // comes from finishSignIn if any errors occur
-  const { error } = useLocalSearchParams();
-  const config = {
-    webClientId,
-    iosClientId,
-    androidClientId,
-  };
-
-  // Request is needed to make google auth work without errors,
-  // but is not explicitly used, hence the override
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [request, response, promptAsync] = Google.useAuthRequest(config);
+  const [driverId, setDriverId] = useState<string>("");
+  const [signedIn, setSignedIn] = useState<boolean | null>(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [netid, setNetid] = useState("");
 
   useEffect(() => {
-    if (response?.type === "success") {
-      const serializedResponse = JSON.stringify(response);
-      console.log(serializedResponse);
-      console.log("OAuth code received. Navigating to redirect page.");
-      // Passes the code to finishSignIn, which handles the Websocket exchange.
-      router.replace({
-        pathname: "/(student)/finishSignIn",
-        params: { serializedResponse },
+    const connectWebSocket = async () => {
+      // call our new route
+      const msg: WebsocketConnectMessage = await WebSocketService.connect()
+        .then((msg) => msg)
+        .catch((err) => err);
+      if (msg !== "Connected Successfully") {
+        console.log(
+          "WEBSOCKET: Failed to connect to WebSocket in Driver SignIn"
+        );
+      }
+    };
+    connectWebSocket();
+    WebSocketService.addListener(handleSigninMessage, "SIGNIN");
+  }, []);
+
+  const checkDriverIdInput = () => {
+    if (/^[a-z0-9]+$/.test(driverId.toLowerCase())) {
+      setNetid(driverId.toLowerCase());
+      setErrMsg("");
+      WebSocketService.send({
+        directive: "SIGNIN",
+        response: null,
+        role: "DRIVER",
+        netid: driverId.toLowerCase(),
       });
+    } else {
+      setDriverId("");
+      setErrMsg("Driver ID must have only lowercase letters or numbers.");
     }
-  }, [response]);
+  };
 
-  return (
-    <SafeAreaView style={[styles.container, { padding: 20 }]}>
-      <View style={{ flex: 1, width: "100%", justifyContent: "space-between" }}>
-        <View style={{ alignSelf: "flex-start" }}>
-          <Link href="/driverOrstudent" asChild>
-            <Pressable>
-              <Ionicons name="arrow-back" size={40} color="#4B2E83" />
-            </Pressable>
-          </Link>
-        </View>
-        {/* Main Content */}
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+  const handleSigninMessage = (message: WebSocketResponse) => {
+    if ("response" in message && message.response == "SIGNIN") {
+      setSignedIn(true);
+    } else {
+      const errorResp = message as ErrorResponse;
+      setErrMsg(errorResp.error);
+      setSignedIn(false);
+    }
+  };
+
+  return signedIn && netid ? (
+    <Redirect
+      href={{
+        pathname: "/(driver)/home",
+        params: { netid: netid },
+      }}
+    />
+  ) : (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          paddingHorizontal: 40,
+          backgroundColor: "#fff",
+        }}
+        behavior="padding"
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "700",
+            color: "#4B2E83",
+            textAlign: "left",
+          }}
         >
-          <Text style={[styles.heading, { marginBottom: "5%" }]}>
-            Welcome Student!
-          </Text>
-          <Image
-            style={[
-              styles.signInbottomImageContainer,
-              { flex: 0.5, marginBottom: "5%" },
-            ]}
-            source={huskyCarImage}
-            resizeMode="contain"
-          />
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: "center",
-              fontWeight: "500",
-              color: "#4B2E83",
-              lineHeight: 30,
-              marginVertical: 20,
-            }}
-          >
-            Start your SafeTrip journey by signing in with your UW email
-          </Text>
+          Driver’s Log In
+        </Text>
 
-          <TouchableOpacity
+        <Text
+          style={{
+            marginTop: 8,
+            fontSize: 14,
+            color: "#555",
+            textAlign: "left",
+          }}
+        >
+          Start your shift by entering your UWPD Driver ID.
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "#ddd",
+            borderRadius: 12,
+            paddingHorizontal: 12,
+            marginTop: 30,
+            width: "100%",
+            height: 70,
+          }}
+        >
+          <Ionicons name="person-outline" size={20} color="#7D5BA6" />
+          <TextInput
+            value={driverId}
             style={{
-              borderColor: "#4B2E83",
-              borderWidth: 2,
-              height: 50,
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: 10,
-              width: "95%",
-              flexDirection: "row",
+              flex: 1,
+              marginLeft: 8,
+              height: 60,
+              fontSize: 16,
+              color: "#000",
             }}
-            onPress={() => promptAsync()}
-          >
-            <Image style={styles.signInGoogleLogo} source={logo} />
-            <Text style={{ fontWeight: "bold", fontSize: 17, marginLeft: 30 }}>
-              Sign in with UW Email
-            </Text>
-          </TouchableOpacity>
-          <Text style={{ color: "red", marginTop: 10 }}>{error}</Text>
+            placeholder="Enter Your Driver ID"
+            placeholderTextColor="#808080"
+            onChangeText={setDriverId}
+            autoCapitalize="none"
+          />
         </View>
-      </View>
-    </SafeAreaView>
+
+        {errMsg ? (
+          <Text style={{ marginTop: 10, color: "red", fontSize: 13 }}>
+            {errMsg}
+          </Text>
+        ) : null}
+
+        <Pressable
+          style={{
+            backgroundColor: driverId.length > 0 ? "#4B2E83" : "gray",
+            borderRadius: 16,
+            marginTop: 280,
+            width: "100%",
+            height: 70,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          disabled={!driverId.length}
+          onPress={checkDriverIdInput}
+        >
+          <Text style={{ color: "white", fontSize: 18, fontWeight: "600" }}>
+            Log In
+          </Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
